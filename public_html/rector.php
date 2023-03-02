84bbc98c5d4fa88856afd2dcbb03aa45ddf45a7b
Ernest Buffington <ernest.buffington@gmail.com>
2023-03-02 16:53:04 -0500

    Update rector.php

--- public_html/rector.php
+++ public_html/rector.php
@@ -8,13 +8,13 @@ use Rector\Set\ValueObject\LevelSetList;
 
 return static function (RectorConfig $rectorConfig): void {
     $rectorConfig->paths([
-		  __DIR__ . '/install/install.php',
+		  __DIR__ . '/install/install2.php',
     ]);
 
     // register a single rule
     $rectorConfig->rule(InlineConstructorDefaultToPropertyRector::class);
 
         $rectorConfig->sets([
-            LevelSetList::UP_TO_PHP_81
+            LevelSetList::UP_TO_PHP_82
         ]);
 };
