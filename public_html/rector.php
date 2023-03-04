3b7ed021c91a74e48bc2ee0135a693c8265aaa06
Ernest Buffington <ernest.buffington@gmail.com>
2023-03-04 15:47:57 -0500

    Update rector.php

--- public_html/rector.php
+++ public_html/rector.php
@@ -8,7 +8,7 @@ use Rector\Set\ValueObject\LevelSetList;
 
 return static function (RectorConfig $rectorConfig): void {
     $rectorConfig->paths([
-		  __DIR__ . '/modules/Amazon/credits.php',
+		  __DIR__ . '/admin/modules/amazon.php',
     ]);
 
     // register a single rule
