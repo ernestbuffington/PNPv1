e0eec8304e0efa949b8a0981e3e95b351fde95c4
Ernest Buffington <ernest.buffington@gmail.com>
2023-03-07 00:21:04 -0500

    Update theme.php

--- public_html/themes/Mech/theme.php
+++ public_html/themes/Mech/theme.php
@@ -36,155 +36,76 @@ function OpenTable() {
     global $tableStatus;
 
 if ($tableStatus != "open"){
-
     ?>
-
 <br>
-
 <table border="0" align=center cellpadding="0" cellspacing="0" width="98%">
-
   <!--DWLayoutTable-->
-
   <tr> 
-
     <td width="23" height="23" valign="top"><img src="themes/Mech/images/ttl.jpg" width="23" height="23"></td>
-
     <td width="100%" valign="top"><table width="100%" border="0" cellpadding="0" cellspacing="0" background="themes/Mech/images/ttm.jpg">
-
         <!--DWLayoutTable-->
-
         <tr> 
-
           <td width="712" height="23" valign="top"><!--DWLayoutEmptyCell-->&nbsp;</td>
-
         </tr>
-
       </table></td>
-
     <td width="23" valign="top"><img src="themes/Mech/images/ttr.jpg" width="23" height="23"></td>
-
     <td width="1"></td>
-
   </tr>
-
   <tr> 
-
     <td height="24" valign="top" background="themes/Mech/images/tl.jpg"><img name="leftside" src="themes/Mech/images/spacer.gif" width="1" height="1" border="0" alt=""></td>
-
     <td valign="top" bgcolor="#2e2e2e"> 
-
       <?
-
-
-
 $tableStatus = "open";
-
-
-
 }
-
 else {}
-
 }
-
-
 
 function OpenTable2() {
-
     global $bgcolor1, $bgcolor2;
-
     echo "<table width=\"100%\" border=\"0\" cellspacing=\"1\" cellpadding=\"0\" ><tr><td class=row1>\n";
-
     echo "<table width=\"100%\" border=\"0\" cellspacing=\"1\" cellpadding=\"8\" ><tr><td>\n";
-
 }
-
-
 
 function CloseTable() {
-
     global $tableStatus;
-
 if ($tableStatus == "open"){
-
     ?>
-
     </td>
-
     <td valign="top" background="themes/Mech/images/tr.jpg"><img name="rightside" src="themes/Mech/images/spacer.gif" width="1" height="1" border="0" alt=""></td>
-
     <td></td>
-
   </tr>
-
   <tr> 
-
     <td height="23" valign="top"><img src="themes/Mech/images/tbl.jpg" width="23" height="23"></td>
-
     <td valign="top"><table width="100%" border="0" cellpadding="0" cellspacing="0" background="themes/Mech/images/tbm.jpg">
-
         <!--DWLayoutTable-->
-
         <tr> 
-
           <td width="712" height="23" valign="top"><!--DWLayoutEmptyCell-->&nbsp;</td>
-
         </tr>
-
         <!--DWLayoutTable-->
-
       </table></td>
-
     <td valign="top"><img src="themes/Mech/images/tbr.jpg" width="23" height="23"></td>
-
     <td></td>
-
   </tr>
-
   <tr> 
-
     <td height="1"><img src="themes/Mech/images/Hm.jpg" alt="" width="23" height="1"></td>
-
     <td></td>
-
     <td><img src="themes/Mech/images/Hm.jpg" alt="" width="23" height="1"></td>
-
     <td><img src="themes/Mech/images/Hm.jpg" alt="" width="1" height="1"></td>
-
   </tr>
-
 </table>
-
 <?
-
-
-
 $tableStatus = "closed";
-
 }
-
 else {}
-
 }
-
-
 
 function CloseTable2() {
-
     echo "</td></tr></table></td></tr></table>\n";
-
 }
 
-
-
 /************************************************************/
-
 /* FormatStory                                              */
-
 /************************************************************/
-
-
-
 function FormatStory($thetext, $notes, $aid, $informant) {
 
     global $anonymous;
@@ -192,43 +113,25 @@ function FormatStory($thetext, $notes, $aid, $informant) {
     $notes = $notes != "" ? "<br><br><b>"._NOTE."</b> <i>$notes</i>\n" : "";
 
     if ("$aid" == "$informant") {
-
         echo "<font class=\"content\" color=\"#000000\">$thetext$notes</font>\n";
-
     } else {
-
         if($informant != "") {
-
             $boxstuff = "<a href=\"modules.php?name=Your_Account&amp;op=userinfo&amp;username=$informant\">$informant</a> ";
-
         } else {
-
             $boxstuff = "$anonymous ";
-
         }
-
         $boxstuff .= ""._WRITES." <i>\"$thetext\"</i>$notes\n";
-
         echo "<font class=\"content\" color=\"#000000\">$boxstuff</font>\n";
-
     }
-
 }
 
-
-
 /************************************************************/
-
 /* Function themeheader()                                   */
-
 /************************************************************/
-
     function themeheader() {
 
     $r_file = null;
     global $user, $cookie, $sitekey, $prefix, $name, $db;
-
-    
 
     cookiedecode($user);
 
@@ -247,38 +150,23 @@ function FormatStory($thetext, $notes, $aid, $informant) {
     $username = $cookie[1];
 
     if ($username == "") {
-
         $username = "Anonymous";
-
     }
 
-    
-
     if ($username == "Anonymous") {
-
         $theuser = "<form action=\"modules.php?name=Your_Account\" method=\"post\"><input type=\"text\" name=\"username\" value=\"username\" onFocus=\"if(this.value=='username')this.value='';\" value style=\"width:90;height:18;\" class=1>
-
   <input type=\"password\" name=\&quot;user_password\&quot; value=\"password\" onFocus=\"if(this.value=='password')this.value='';\" style=\"width:90;height:18;\" class=1>
-
   <input type=\"hidden\" name=\"random_num\" value=\"$random_num\">
-
   <input type=\"hidden\" name=\"gfx_check\" value=\"$code\">
-
   <input type=\"hidden\" name=\"op\" value=\"login\">
-
   <input type=\"image\" value=\"login\" class=\"noborder\" src=\"themes/Mech/images/login.gif\" border=\"0\" alt=login>
-
   <a href=\"modules.php?name=Your_Account&op=new_user\"><img src=\"themes/Mech/images/reg.gif\" border=0 alt=register></a></td> 
-
 </form>
 
 <p>\n"; } else { $theuser = "<img src=\"themes/Mech/images/spacer.gif\" border=0 width=4 height=1><font class=copyright>Welcome 
 
   $username</font><a href=\"modules.php?name=Your_Account&op=logout\"><img src=\"themes/Mech/images/logout.gif\" border=0 alt=logout></a></TD>\n"; 
-
   }
-
-    
 
 $sql = "SELECT msg1, msg2, msg3, link1, link2, link3, link4, link5, link1url, link2url, link3url, link4url, link5url, searchbox, flash FROM ".$prefix."_themecp";
 
@@ -304,86 +192,40 @@ $public_msg = public_message();
 
 echo "$public_msg";
 
-
-
 if ($searchbox == 1) {	
-
 	$search1 ="<form action=\"modules.php?op=modload&name=Search&file=index\" method=\"post\"><input type=\"text\" name=\"query\" value=\"type search here\" onFocus=\"if(this.value=='type search here')this.value='';\" value style=\"width:120;height:18;\" class=1>&nbsp;<input type=\"image\" class=\"noborder\" value=\"search\" src=\"themes/Mech/images/search.gif\" border=\"0\" alt=\"submit search\"></TD></form>\n";
-
 } else {
-
 	$search1 ="&nbsp;</td>\n";
-
 }
-
-
-
-    
-
-
-
    echo "<body topmargin=\"0\" leftmargin=\"0\" marginwidth=\"0\" marginheight=\"0\">";
-
-
-
     $tmpl_file = "themes/Mech/header.html";
-
     $thefile = implode("", file($tmpl_file));
-
     $thefile = addslashes($thefile);
-
     $thefile = "\$r_file=\"".$thefile."\";";
-
     eval($thefile);
-
     print $r_file;
-
-    
-
-
-
 //LEFT SIDE BACKGROUND
 
 echo "<table width=\"100%\" cellpadding=\"0\" cellspacing=\"0\" border=\"0\" align=\"center\">\n"
-
 		."<tr valign=\"top\">\n"
-
         ."<td width=\"31\" valign=\"top\" background=\"themes/Mech/images/leftB.jpg\"><img src=\"themes/Mech/images/leftB.jpg\" width=\"31\" height=\"200\" border=\"0\"></td>\n"
-
 		."<td width=\"165\" valign=\"top\">\n";
 
     if ($name=='Forums' || $name=='Private_Messages' || $name=='Members_List' || $name=='Shopping_Cart' || $name=='Your_Account') { 
-
 } else { 
-
 blocks('left'); 
-
 } 
-
     echo "</td>\n"
-
     	."<td width=\"0\" valign=\"top\" background=\"themes/Mech/images/spacer2.gif\"><img src=\"themes/Mech/images/spacer2.gif\" width=\"0\" height=\"1\" border=\"0\"></td>\n"
-
     	."<td width=\"100%\">\n";
-
 }
 
-
-
 /************************************************************/
-
 /* Function themefooter()                                   */
-
 /************************************************************/
-
-
-
 $foot1 = "test";
-
 $foot2 = "test";
-
 $foot3 = "test";
-
 $foot4 = "test";
 
 function themefooter() {
@@ -400,8 +242,6 @@ $footer_message = $foot1. "<br>" . $foot2 . "<br>" . $foot3 . "<br>" . $foot4;
 
 $showsub = "<FORM action=modules.php?op=modload&amp;name=Newsletter&amp;file=index&amp;func=action method=post><font class=copyright>&nbsp;<b>Email</b><br>&nbsp;<INPUT maxLength=100 size=20 class=\"sub\" name=new_email value=\"Your Email\" onFocus=\"if(this.value=='Your Email')this.value='';\" value style=\"width:150;height:18;FONT-SIZE: 9px;\"><BR><BR>&nbsp;<SELECT name=new_sub><OPTION value=sub selected>Subscribe<OPTION value=unsub>Unsubscribe</OPTION></SELECT><BR><BR>&nbsp;<SELECT name=new_type><OPTION value=0 selected>Text<OPTION value=1>HTML</OPTION></SELECT><br><br>&nbsp;<INPUT type=image class=\"liteoption1\" value=Submit src=\"themes/MStudioV2/images/submit.gif\" border=\"0\"></font></FORM>";
 
-
-
 $maxshow = 10;	// Number of downloads to dispaly in the block.
 
 $a = 1;
@@ -415,20 +255,10 @@ while([$lid, $title, $hits] = sql_fetch_row($result, $dbi)) {
     $show .= "&nbsp;&nbsp;&nbsp;$a: <a href=\"modules.php?name=Web_Links&amp;l_op=viewlinkdetails&amp;lid=$lid&amp;ttitle=$title\">$title2</a><br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<font class=\"content\">$hits<font class=\"copyright\"> times<br>";
 
      $showlinks = " <A name= \"scrollingCode\"></A><MARQUEE behavior= \"scroll\" align= \"left\" direction= \"up\" width=\"140\" height=\"97\" scrollamount= \"2\" scrolldelay= \"30\" onmouseover='this.stop()' onmouseout='this.start()'>$show";
-
-    
-
-    
-
     $a++;
-
 }
 
-
-
 global $prefix, $db;
-
-
 
 $a = 1;
 
@@ -441,27 +271,16 @@ while ($row = $db->sql_fetchrow($result)) {
     $title2 = preg_replace('#_#m', " ", (string) $row[\TITLE]);
 
    // $content .= "<strong><big>&middot;</big></strong>&nbsp;$a: <a href=\"modules.php?name=Downloads&amp;d_op=viewdownloaddetails&amp;lid=$row[lid]&amp;title=$row[title]\">$title2</a><br>";
-
 	$content .= "<strong><big>&middot;</big></strong>&nbsp;<a href=\"modules.php?name=Downloads&amp;d_op=viewdownloaddetails&amp;lid=$row[lid]&amp;title=$row[title]\"><img src=\"themes/Mech/images/spacer.gif\" border=0></a> $a: <a href=\"modules.php?name=Downloads&amp;d_op=viewdownloaddetails&amp;lid=$row[lid]&amp;title=$row[title]\">$title2</a></span><br>";
 
 	 $showdl = " <font class=copyright>&nbsp;</b><br>&nbsp;<A name= \"scrollingCode\"></A><MARQUEE behavior= \"scroll\" align= \"left\" direction= \"up\" width=\"122\" height=\"91\" scrollamount= \"2\" scrolldelay= \"30\" onmouseover='this.stop()' onmouseout='this.start()'>$content";
-
-
-
     $a++;
-
 }
 
 if ($banners == 1) {
-
     $numrows = $db->sql_numrows($db->sql_query("SELECT * FROM ".$prefix."_banner WHERE type='0' AND active='1'"));
-
    /* Get a random banner if exist any. */ 
-
    /* More efficient random stuff, thanks to Cristian Arroyo from http://www.planetalinux.com.ar */ 
-
-
-
     if ($numrows>1) { 
 
    $numrows -= 1; 
@@ -490,8 +309,6 @@ if ($banners == 1) {
 
     $alttext = $row[\ALTTEXT]; 
 
-    
-
     if (!is_admin($admin)) { 
 
        $db->sql_query("UPDATE ".$prefix."_banner SET impmade=impmade+1 WHERE bid='$bid'"); 
@@ -516,12 +333,7 @@ if ($banners == 1) {
 
    $date = $row2[\DATE]; 
 
-
-
 /* Check if this impression is the last one and print the banner */ 
-
-
-
    if ($imptotal <= $impmade && $imptotal != 0) { 
 
        $db->sql_query("UPDATE ".$prefix."_banner SET active='0' WHERE bid='$bid'"); 
@@ -581,75 +393,39 @@ if ($banners == 1) {
     }
 
 }
-
-
-
-
-
     if ($index == 1) {
 
 echo'</td>    <td width="170" valign="top">';
 
-
-
-
-
-    blocks(\RIGHT);
-
-
+    blocks('right');
 
 	}
 
 echo"</td>"
-
   . "    <td width=\"31\" valign=\"top\" background=\"themes/Mech/images/rightB.jpg\"><img src=\"themes/Mech/images/rightB.jpg\" width=\"31\" height=\"30\"></td>"
-
   . "  </tr>"
-
   . "</table>"
-
  ."";
-
-    
-
-   
-
     include(__DIR__ . "/themes/Mech/footer.php");
-
-    
-
 }
 
-
-
 /************************************************************/
-
 /* Function themeindex()                                    */
-
 /* This function format the stories on the Homepage         */
-
 /************************************************************/
-
 function themeindex ($aid, $informant, $time, $title, $counter, $topic, $thetext, $notes, $morelink, $topicname, $topicimage, $topictext) {
 
     $timezone = null;
     $r_file = null;
     global $anonymous, $tipath;
 
-
-
 $ThemeSel = get_theme();
 
     if (file_exists("themes/$ThemeSel/images/topics/$topicimage")) {
-
         $t_image = "themes/$ThemeSel/images/topics/$topicimage";
-
     } else {
-
         $t_image = "$tipath$topicimage";
-
 }
-
     $notes = $notes != "" ? "<br><br><b>"._NOTE."</b> $notes\n" : "";
 
     if ("$aid" == "$informant") {
@@ -696,16 +472,9 @@ $ThemeSel = get_theme();
 
 }
 
-
-
 /************************************************************/
-
 /* Function themeindex()                                    */
-
 /************************************************************/
-
-
-
 function themearticle ($aid, $informant, $datetime, $title, $thetext, $topic, $topicname, $topicimage, $topictext) {
 
     $notes = null;
@@ -765,33 +534,18 @@ $ThemeSel = get_theme();
 
 }
 
-
-
 /************************************************************/
-
 /* Function themesidebox()                                  */
-
 /************************************************************/
-
-
-
 function themesidebox($title, $content) {
 
     $r_file = null;
     $tmpl_file = "themes/Mech/blocks.html";
-
     $thefile = implode("", file($tmpl_file));
-
     $thefile = addslashes($thefile);
-
     $thefile = "\$r_file=\"".$thefile."\";";
-
     eval($thefile);
-
     print $r_file;
-
 }
-
-
 
 ?>
