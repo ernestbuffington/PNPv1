565313a119f23601feacaeaba2d120c019176697
Ernest Buffington <ernest.buffington@gmail.com>
2023-03-08 08:32:29 -0500

    Create mimetype.php

---
+++ public_html/themes/Mech/includes/mimetype.php
@@ -0,0 +1,10 @@
+<?php
+global $ThemeSel;
+echo "\n<!-- Loading MimeType from themes/".$ThemeSel."/mimetype.php -->\n";
+echo '<?xml version="1.0" encoding="UTF-8"?>'."\n";
+echo '<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">'."\n";
+echo "<!-- Loading Header from themes/".$ThemeSel."/mimetype.php START -->\n";
+echo '<head>'."\n";
+echo '<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />'."\n";
+echo "<!-- Loading Header from themes/".$ThemeSel."/mimetype.php END -->\n\n";
+
