a0f67f0961a301d94bc9d7ff96511219bec42c32
Ernest Buffington <ernest.buffington@gmail.com>
2023-03-08 08:32:35 -0500

    Create platinum_footer.php

---
+++ public_html/themes/Mech/platinum_footer.php
@@ -0,0 +1,30 @@
+<?php
+global $locked_width, $ThemeInfo, $sitename, $theme_name;
+
+echo '<div align="center">';
+
+echo '<table width="'.$locked_width.'" height="172" border="0" cellpadding="0" cellspacing="0" background="themes/'.$theme_name.'/images/fm.jpg">';
+echo '<tr>';
+echo '<td width="244" height="175" colspan="1" rowspan="1" valign="top"><table width="244" border="0" cellpadding="0" cellspacing="0" background="themes/'.$theme_name.'/images/fl.jpg">';
+echo '<tr>';
+echo '<td width="72" height="49">&nbsp;</td>';
+echo '<td width="122">&nbsp;</td>';
+echo '<td width="50">&nbsp;</td>';
+echo '</tr>';
+echo '<tr>';
+echo '<td height="103">&nbsp;</td>';
+echo '<td valign="top"><font color="#FFCC00">'.$showdl.'</font></td>';
+echo '<td>&nbsp;</td>';
+echo '</tr>';
+echo '<tr>';
+echo '<td height="23">&nbsp;</td>';
+echo '<td>&nbsp;</td>';
+echo '<td>&nbsp;</td>';
+echo '</tr>';
+echo '</table></td>';
+echo '<td width="100%" align="center" valign="top"><img src="themes/'.$theme_name.'/images/fmm.jpg" width="311" height="175"></td>';
+echo '<td width="240" valign="top"> <img src="themes/'.$theme_name.'/images/fr.jpg" width="240" height="175"></td>';
+echo '</tr><tr><td height="1"><img src="themes/'.$theme_name.'/images/spacer.gif" alt="" width="244" height="1"></td><td></td><td><img src="themes/Mech/'.$theme_name.'/spacer.gif" alt="" width="240" height="1"></td></tr>';
+echo '</rable>';
+
+echo '</div>';
